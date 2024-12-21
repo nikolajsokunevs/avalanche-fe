0b27f1088dbffb25747c1b1b36cc1cf92864764c
nikolajs.okunevs <nikolajs.okunevs@agcsoft.com>
2024-12-22 00:22:39 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://gameavalanche.duckdns.org:8443/api',
-  webSocketUrl: "ws://localhost:8080/ws",
+  apiUrl: 'https://gameavalanche.duckdns.org/api',
+  webSocketUrl: "https://gameavalanche.duckdns.org/ws",
   useMockTelegram: false
 };
